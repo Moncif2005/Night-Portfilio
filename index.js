efdb9fccb050dead539aad687df9fd5f5ef1bcb7
abdelilleh <143023531+Moncif2005@users.noreply.github.com>
2024-03-24 19:08:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const script = document.createElement("script");
+        script.src = "scrollreveal.min.js";
+        script.onload = function () {
+            const sr = ScrollReveal({
+                origin: 'left',
+                distance: '90px',
+                duration: 3000,
+                delay: 450,
+                reset: false, 
+            });
+
+            sr.reveal('.navbar', { delay: 400 , origin:'top' });
+            sr.reveal('.text-part1', { delay: 410, origin: 'left' });
+            sr.reveal('.paragraph', { delay: 440 ,origin: 'left' });
+            sr.reveal('.buttons', { delay: 470 ,origin: 'left'});
+            sr.reveal('.icons-social', { delay: 500 });
+            sr.reveal('.image', { delay: 500 ,origin: 'right'});
+
+            sr.reveal('.image-about', { delay: 510,origin: 'left' });
+            sr.reveal('.text-about', { delay: 520, origin:'right' });
+            sr.reveal('.about-title', { delay: 530 });
+            
+
+            sr.reveal('.container', { delay: 510,origin: 'right' });
+            sr.reveal('.container-2', { delay: 520, origin:'left' });
+            sr.reveal('.skills-title', { delay: 530 });
+
+
+            sr.reveal('.services-title', { delay: 530, });
+            sr.reveal('.ser-btn', { delay: 510,origin: 'right' });
+            sr.reveal('.small-title', { delay: 520, origin:'left' });
+            sr.reveal('.project', { delay: 510,origin: 'right' });
+
+
+            sr.reveal('.Contact-title', { delay: 530, });
+            sr.reveal('.contact-section', { delay: 510, origin: 'right' });
+            sr.reveal('.contact-img', { delay: 410, origin: 'bottom' });
+
+            sr.reveal('.footer-section', { delay: 410, origin: 'bottom' });
+        };
+
+        document.head.appendChild(script);
+
+
+
+        const list = document.querySelector(".project");
+        const list_2 = document.querySelector(".project-2");
+        
+        const left_btn = document.querySelector(".left-side"); 
+        const right_btn = document.querySelector(".right-side"); 
+
+        left_btn.addEventListener("click", () => {
+            list.style.display = "flex";
+            list_2.style.display = "none";
+        });
+        
+        
+        right_btn.addEventListener("click", () => {
+            list.style.display = "none";
+            list_2.style.display = "flex";
+        });
+        
